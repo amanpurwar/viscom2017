87c2f3c170c3e34484bd1fbdcb71c0cc573a084d
amanpurwar <amanpurwar35@gmail.com>
2017-03-28 00:20:41 +0530

    Completed the subplot part TODO:plot of maximum emotion of every time instant

--- Indico.py
+++ Indico.py
@@ -6,9 +6,9 @@ indicoio.config.api_key = '929ac1b1d3eac44546d60bf0a359816a'
 # single example                                                                                                               
 lx=[]
 angry=[];sad=[];neutral=[];surprise=[];happy=[];fear=[];
-for i in range(1,5):
+for i in range(1,10):
         lx.append(i)
-for i in range(1,5):
+for i in range(1,10):
         print("example("+str(i)+")")
         tmp = indicoio.fer("imgs/face_"+str(i)+".jpg")
         print(tmp)
@@ -16,26 +16,34 @@ for i in range(1,5):
 	happy.append(tmp[u'Happy']);fear.append(tmp[u'Fear']);
 
 plt.figure(1)
-plt.plot(lx,angry, 'ro', linewidth=2.0)
-plt.axis([0,5, 0, 1])
+ax=plt.subplot(221)
+ax.set_title("angry")
+plt.plot(lx,angry, 'ro-', linewidth=2.0)
+plt.axis([0,10, 0, 1])
 
-plt.subplot(220)
-plt.plot(lx,sad, 'bo', linewidth=2.0)
-plt.axis([0, 5, 0, 1])
+ax=plt.subplot(222)
+ax.set_title("sad")
+plt.plot(lx,sad, 'bo-', linewidth=2.0)
+plt.axis([0, 10, 0, 1])
 
-plt.subplot(222)
-plt.plot(lx,neutral, 'go', linewidth=2.0)
-plt.axis([0, 5, 0, 1])
+ax=plt.subplot(223)
+ax.set_title("neutral")
+plt.plot(lx,neutral, 'go-', linewidth=2.0)
+plt.axis([0, 10, 0, 1])
 
-plt.subplot(223)
-plt.plot(lx,surprise, 'yo', linewidth=2.0)
-plt.axis([0, 5, 0, 1])
+ax=plt.subplot(224)
+ax.set_title("surprise")
+plt.plot(lx,surprise, 'yo-', linewidth=2.0)
+plt.axis([0, 10, 0, 1])
 
-plt.subplot(224)
-plt.plot(lx,happy, 'mo', linewidth=2.0)
-plt.axis([0, 5, 0, 1])
+plt.figure(2)
+ax=plt.subplot(221)
+ax.set_title("happy")
+plt.plot(lx,happy, 'mo-', linewidth=2.0)
+plt.axis([0, 10, 0, 1])
 
-plt.subplot(225)
-plt.plot(lx,fear, 'ko', linewidth=2.0)
-plt.axis([0, 5, 0, 1])
+ax=plt.subplot(222)
+ax.set_title("fear")
+plt.plot(lx,fear, 'ko-', linewidth=2.0)
+plt.axis([0, 10, 0, 1])
 plt.show()
